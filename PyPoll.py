fc4ca0688daa78ec40bd3147d75c163be083d891
emoodyv <colliermoody5@gmail.com>
2020-09-27 15:00:28 -0700

    Election Analysis

--- PyPoll.py
+++ PyPoll.py
@@ -14,6 +14,12 @@ for i in range(0, len(df.index)):
     if df[headers[2]][i] not in canidate_options:
         canidate_options.append(df[headers[2]][i])
 
+# Getting the counties
+county_options = []
+for i in range(0, len(df.index)):
+    if df[headers[1]][i] not in county_options:
+        county_options.append(df[headers[1]][i])
+
 # Getting the amount of votes per candidates
 canidate_votes = {canidate_options[0]: 0, canidate_options[1]: 0, canidate_options[2]: 0}
 for i in range(0, len(df.index)):
@@ -24,11 +30,25 @@ for i in range(0, len(df.index)):
     elif df[headers[2]][i] == canidate_options[2]:
         canidate_votes[canidate_options[2]] += 1
 
+# Getting the amount of votes per county
+county_votes = {county_options[0]: 0, county_options[1]: 0, county_options[2]: 0}
+for i in range(0, len(df.index)):
+    if df[headers[1]][i] == county_options[0]:
+        county_votes[county_options[0]] += 1
+    elif df[headers[1]][i] == county_options[1]:
+        county_votes[county_options[1]] += 1
+    elif df[headers[1]][i] == county_options[2]:
+        county_votes[county_options[2]] += 1
+
 # Getting the percentages
-vote_percentage = []
-vote_percentage.append(canidate_votes[canidate_options[0]] / total_votes * 100)
-vote_percentage.append(canidate_votes[canidate_options[1]] / total_votes * 100)
-vote_percentage.append(canidate_votes[canidate_options[2]] / total_votes * 100)
+vote_percentage = [canidate_votes[canidate_options[0]] / total_votes * 100,
+                   canidate_votes[canidate_options[1]] / total_votes * 100,
+                   canidate_votes[canidate_options[2]] / total_votes * 100]
+
+# Getting the county percentages
+county_percentage = [county_votes[county_options[0]] / total_votes * 100,
+                     county_votes[county_options[1]] / total_votes * 100,
+                     county_votes[county_options[2]] / total_votes * 100]
 
 # Winning Candidate and Winning Count Tracker
 winner_index = np.int(np.where(vote_percentage == np.amax(vote_percentage))[0])
@@ -36,20 +56,42 @@ winning_candidate = canidate_options[winner_index]
 winning_count = '{:,}'.format(canidate_votes[canidate_options[winner_index]])
 winning_percentage = '{:.1f}'.format(vote_percentage[winner_index])
 
+# Highest turnout county
+highest_county_index = np.int(np.where(county_percentage == np.amax(county_percentage))[0])
+highest_county_name = county_options[highest_county_index]
+highest_county_count = '{:,}'.format(county_votes[county_options[highest_county_index]])
+highest_county_percentage = '{:.1f}'.format(county_percentage[highest_county_index])
+
 # Printing the results
-for i in range(0, 3):
+print('Election Results\n-------------------------\nTotal Votes: ' + str('{:,}'.format(int(total_votes))) +
+      '\n-------------------------\nCounty Votes:')
+
+for i in range(0, len(headers)):
+    print(county_options[i] + ': ' + str('{:.1f}'.format(county_percentage[i])) + '% (' +
+          str('{:,}'.format(county_votes[county_options[i]])) + ')')
+
+print('-------------------------\nLargest County Turnout: ' + highest_county_name + '\n-------------------------')
+
+for i in range(0, len(headers)):
     print(canidate_options[i] + ': ' + str('{:.1f}'.format(vote_percentage[i])) + '% (' +
           str('{:,}'.format(canidate_votes[canidate_options[i]])) + ')')
 
-print('\n-------------------------\nWinner: ' + winning_candidate + '\nWinning Vote Count: '
+print('-------------------------\nWinner: ' + winning_candidate + '\nWinning Vote Count: '
       + str(winning_count) + '\nWinning Percentage: ' + str(winning_percentage) +
       '%\n-------------------------\n')
 
 # Recording the results
 f = open('election_analysis.txt', 'w')
-f.write('Election Results\n-------------------------\nTotal Votes: ' + str('{:,}'.format(total_votes)) +
-        '\n-------------------------\n')
-for i in range(0, 3):
+f.write('Election Results\n-------------------------\nTotal Votes: ' + str('{:,}'.format(int(total_votes))) +
+        '\n-------------------------\nCounty Votes:\n')
+
+for i in range(0, len(headers)):
+    f.write(county_options[i] + ': ' + str('{:.1f}'.format(county_percentage[i])) + '% (' +
+          str('{:,}'.format(county_votes[county_options[i]])) + ')\n')
+
+f.write('-------------------------\nLargest County Turnout: ' + highest_county_name + '\n-------------------------\n')
+
+for i in range(0, len(headers)):
     f.write(canidate_options[i] + ': ' + str('{:.1f}'.format(vote_percentage[i])) + '% ('
             + str('{:,}'.format(canidate_votes[canidate_options[i]])) + ')\n')
 
